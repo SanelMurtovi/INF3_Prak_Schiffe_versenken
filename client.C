f76090e696a02d096b876d7f36cd50c8a9867141
HartfelderJustin <justin.hartfelder.arb@gmail.com>
2025-02-14 20:49:26 +0100

    Add files via upload

--- client.C
+++ client.C
@@ -1,18 +1,141 @@
-/*
- * client.C
- *
- *  Created on: 11.09.2019
- *      Author: aml
- */
-
 #include <string>
 #include <iostream>
 #include <unistd.h> //contains various constants
+#include <random>
+#include <set>
+#include <fstream>
 
 #include "SIMPLESOCKET.H"
 
 using namespace std;
 
+
+TCPclient c;
+
+int tactic1();
+int tactic2();
+int tactic3();
+
+
+unsigned char attemps = 0;
+unsigned int tries = 100;
+
+
+int main(){
+    //Client Setup
+    cout << "Client initiated" << endl;
+    srand(time(NULL));
+    string host = "localhost";
+
+    unsigned char atmpsArr[tries];
+
+    cout << "Establishing Connection ..." << endl;
+    c.conn(host, 2027);
+
+    //Statistical Data File
+    ofstream StatisticalData;
+    StatisticalData.open("StatisticalData.csv");
+
+    if(StatisticalData.is_open()){
+        cout << "The File for the saving of statistical Data was openend sucessfully, Proceeding ..." << endl;
+    }
+    else{
+        cout << "Unexpected Error opening the file" << endl;
+    }
+
+    //Strategy Request
+    int strategy;
+    cout << "What Strategy should be statistically analysed ?:" << endl;
+    cout << "1. Strategy 1" << endl;
+    cout << "2. Strategy 2" << endl;
+    cout << "3. Strategy 3" << endl;
+    cin >> strategy;
+
+    switch(strategy){
+        case 1: cout << "Strategy 1 has been choosen" << endl;
+                break;
+        case 2: cout << "Strategy 2 has been choosen" << endl;
+                break;
+        case 3: cout << "Strategy 3 has been choosen" << endl;
+                break;
+        default:
+                cout << "Invalid Strategy!" << endl;
+                sleep(15);
+                return 0;
+        }
+
+     for(int n = 1; n<=tries; n++){
+        atmpsArr[n] = 0;
+        c.sendData("NEWGAME");
+        string msg = c.receive(32);
+        int currRun = 0;
+
+        switch(strategy){
+        case 1: currRun = tactic1();
+                break;
+        case 2: currRun = tactic2();
+                break;
+        case 3: currRun = tactic3();
+                break;
+        default:
+                cout << "Invalid Strategy!" << endl;
+                sleep(15);
+                return 0;
+        }
+        atmpsArr[n] = currRun;
+        if(atmpsArr[n] == 0){
+            cout << "ERROR! No Strategy is implemented, terminating ..." << endl;
+            sleep(15);
+            return 0;
+        }
+        StatisticalData << n << ";" << currRun << endl;
+        cout << "Nmb" << n << ": " << to_string(atmpsArr[n]) << endl;
+    }
+
+    StatisticalData.close();
+    if(StatisticalData.is_open() == false){
+            cout << "Statistical Data exported" << endl;
+    }
+    else{
+            cout << "Unknown Error while trying to export Statistical Data" << endl;
+    }
+
+    sleep(50);
+
+    return 0;
+
+}
+
+
+
+int tactic1(){
+    string msg;
+    int attemps = 0;
+    for(int i = 1; i <= 10; i++){
+        for(int j = 1; j <= 10; j++){
+            attemps++;
+            msg = "COORD[" + to_string(j) + ";" + to_string(i) + "]";
+            c.sendData(msg);
+            msg = c.receive(32);
+            if(msg == "ALL SHIPS DESTROYED, TERMINATING SESSION"){
+                return attemps;
+            }
+        }
+    }
+        return -1;
+}
+
+int tactic2(){
+    return 0;
+}
+
+int tactic3(){
+    return 0;
+}
+
+
+
+/*
 int main() {
 	srand(time(NULL));
 	TCPclient c;
@@ -39,5 +162,5 @@ int main() {
 
 	}
 }
-
+*/
 
--- server.C
+++ server.C
@@ -1,14 +1,9 @@
-/*
- * server.C
- *
- *  Created on: 11.09.2019
- *      Author: aml
- */
 #include <cstdio> // standard input and output library
 #include <cstdlib> // this includes functions regarding memory allocation
 #include <cstring> // contains string functions
 #include <cerrno> //It defines macros for reporting and retrieving error conditions through error codes
 #include <ctime> //contains various functions for manipulating date and time
+#include <sstream>
 
 #include <unistd.h> //contains various constants
 #include <sys/types.h> //contains a number of basic derived types that should be used whenever appropriate
@@ -17,10 +12,101 @@
 #include <netinet/in.h> //contains constants and structures needed for internet domain addresses
 
 #include "SIMPLESOCKET.H"
+#include "TASK3.H"
+#include "TASK3.C"
 
+using namespace std;
+
+class myServerShip : public TCPserver{
+
+public:
+
+    myServerShip(int portNmb, int bSize):TCPserver(portNmb, bSize){
+    world = new TASK3::World();
+    cout <<"Server gestartet!" << endl;
+    cout << "Das Spielfeld ist dimensioniert (10x10), bestehend aus folgenden Schifflaengen: 1xFuenf, 2xVier, 3xDrei und 4xZwei"<<endl;
+    }
+    ~myServerShip(){};
+
+private:
+    myServerShip();
+    TASK3::World *world;
+
+protected:
+
+string myResponse(string input){
+    int x = -1,y = -1;
+    stringstream o;
+
+    if(input.compare(0,7,"NEWGAME") == 0){
+        delete world;
+        world = new TASK3::World();
+        return string("NEWGAME");
+   }
+
+    if(sscanf(input.c_str(), "%d %d", &x, &y) !=2)
+    return string ("ERROR: Invalid input");
+
+    TASK3::ShootResult result = world -> shoot(x,y);
+
+    switch (result){
+        case TASK3::WATER:
+            return "MISS";
+        case TASK3::SHIP_HIT:
+            return "HIT";
+        case TASK3::SHIP_DESTROYED:
+            return "SHIP DESTROYED";
+        case TASK3::ALL_SHIPS_DESTROYED:
+            return "ALL SHIPS DESTROYED, TERMINATING SESSION";
+        default:
+            return "ERROR, UNKNOWN RESULT";
+
+        }
+}
+
+};
+
+
+
+
+/*myServerShip::~myServerShip(){
+}
+
+myServerShip::myServerShip(int portNmb, int bSize) : TCPserver(portNmb, bSize){
+
+}*/
 
 int main(){
 	srand(time(nullptr));
-	TCPserver srv(2022,25);
+	myServerShip srv(2027,25); //Instanziiert mit den parametriseriten Konstruktor
 	srv.run();
 }
+
+
+/*protected:
+string myResponse(string input) override {
+    int x= -1, y = -1;
+
+    if(input.compare(0,7,"NEWGAME")==0){
+    delete world;
+    world = new TASK3::World();
+    return string("DONE");
+    }
+
+    if(input.compare(0,6,"COORD[")==0)[
+        if (sscanf (input.c str(), "COORD[%d,%d], &x, &y) !=2){
+            return string("Error. Could not read coord-data.");
+            }
+
+ if(input.compare(0,6,"COORD[") == 0){
+        e = sscanf(input.c_str(),"COORD[%i,%i]",&x,&y);
+        if(e != 2){
+            return string("COULD NOT READ COORDINATES.");
+        }
+        o << "SUMME[" << (x+y) << "]";
+        return (o.str());
+    }
+
+    return string("#"+input+"#");
+}
+}*/
